31e0ff92c6944959ddcc1ad984611d85fc91f3bc
Anna <a.lipetska@gmail.com>
2021-04-18 17:51:41 +0300

    lesson11

---
+++ lesson11/task1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    // Используя конструкции блокировки, создайте метод, который будет в цикле for (допустим, на 10
+    // итераций) увеличивать счетчик на единицу и выводить на экран счетчик и текущий поток.
+    // Метод запускается в трех потоках. Каждый поток должен выполниться поочередно, т.е.в
+    // результате на экран должны выводиться числа (значения счетчика) с 1 до 30 по порядку, а не в
+    // произвольном порядке.
+    class Program
+    {
+        static object block = new object();
+        static int counter;
+        static void Method()
+        {
+            lock (block)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"Поток {Thread.CurrentThread.ManagedThreadId}, счетчик - {++counter}");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[3];
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(Method);
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lesson11/task2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    // Создайте консольное приложение, которое в различных потоках сможет получить доступ к 2-м
+    // файлам.Считайте из этих файлов содержимое и попытайтесь записать полученную
+    // информацию в третий файл.Чтение/запись должны осуществляться одновременно в каждом
+    // из дочерних потоков.Используйте блокировку потоков для того, чтобы добиться корректной
+    // записи в конечный файл.
+    class Program
+    {
+        static object block = new object();
+        static StreamWriter writer = File.CreateText("FoodAndDrinks.txt");
+
+        static void AddDrinksToList()
+        {
+            string drinks;
+
+            using (var reader = File.OpenText("Drinks.txt"))
+            {
+                drinks = reader.ReadToEnd();
+            }
+
+            lock (block)
+            {
+                writer.WriteLine(drinks);
+                writer.WriteLine(new string('-', 30));
+            }
+        }
+
+        static void AddFoodToList()
+        {
+            string food;
+
+            using (var reader = File.OpenText("Food.txt"))
+            {
+                food = reader.ReadToEnd();
+            }
+
+            lock (block)
+            {
+                writer.WriteLine(food);
+                writer.WriteLine(new string('-', 30));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var drinksThread = new Thread(AddDrinksToList);
+            var foodThread = new Thread(AddFoodToList);
+
+            using (writer)
+            {
+                drinksThread.Start();
+                foodThread.Start();
+
+                drinksThread.Join();
+                foodThread.Join();
+            }
+
+            Console.WriteLine("All items are on the same list now.");
+            Console.ReadKey();
+        }
+    }
+}
